a2d2160fe7d06e4920bf10de4b10aa502da16415
cprice <cprice@tutanota.com>
2020-08-25 22:49:44 +0200

    Update apply script

--- apply.sh
+++ apply.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-if [ -z "$(which stow |grep 'not found')" ]
+if ! command -v "stow" &> /dev/null 
 then
-	stow -d "/home/$USER/sources/dotfiles" -t "/home/$USER/.config" ".config"
+	echo "[00F][error] - stow is missing. Install it through your distro repositories."
+	exit 1
+fi
+
+stow -d "/home/$USER/sources/dotfiles" -t "/home/$USER/.config" ".config"
+
+if [ $? == 0 ]
+then
 	echo "[00F][success] - dotfiles have been sucessfully applied."
 else
-	echo "[00F][error] - stow is missing. Install it through your distro repositories."
+	echo "[00F][error] - errors occured. Please check console logs."
 fi
 
