019bc4067db4c4a8eac2db0284feb213d0f59444
cprice <cprice@tutanota.com>
2021-04-05 12:07:28 +0200

    wip

---
+++ .config/eww/weather-icon.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+case $1 in
+    113) echo '' ;;  #  Sunny
+    116) echo '';;  #  Partly cloudy
+    119) echo '' ;;  #  Cloudy
+    122) echo '' ;;  #  Overcast
+    143) echo '' ;;  #  Mist
+    176) echo '' ;;  #  Patchy rain possible
+    179) echo '' ;;  #  Patchy snow possible
+    182) echo '' ;;  #  Patchy sleet possible
+    185) echo '';;  #  Patchy freezing drizzle possible
+    200) echo '' ;;  #  Thundery outbreaks possible
+    227) echo '' ;;  #  Blowing snow
+    230) echo '' ;;  #  Blizzard
+    248) echo '' ;;  #  Fog
+    260) echo '' ;;  #  Freezing fog
+    263) echo '' ;;  #  Patchy light drizzle
+    266) echo '' ;;  #  Light drizzle
+    281) echo '' ;;  #  Freezing drizzle
+    284) echo '' ;;  #  Heavy freezing drizzle
+    293) echo '' ;;  #  Patchy light rain
+    296) echo '' ;;  #  Light rain
+    299) echo '' ;;  #  Moderate rain at times
+    302) echo '' ;;  #  Moderate rain
+    305) echo '' ;;  #  Heavy rain at times
+    308) echo '' ;;  #  Heavy rain
+    311) echo '';;  #  Light freezing rain
+esac
