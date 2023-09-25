b593eab5d54283375f556ccb200e13cead8a1355
noBrainSkull <sorian.slimani@protonmail.ch>
2023-09-25 16:30:41 +0200

    wipe

--- init_vim_config/nvim/lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
--- Install all the plugins
-require('list')
-
--- Configure them
-require('plugin-config')
--- init_vim_config/nvim/lua/plugins/list.lua
+++
@@ -1,7 +0,0 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- Catppuccin !
-  use { "catppuccin/nvim", as = "catppuccin" }
-end)
